35d80b6357d9ca02142749f253027bf475981499
Ben Goodwyn <ben@clouddirective.com>
2017-12-03 22:09:22 -0600

    :white_check_mark: Re-write day 1 tests using fluent asserts

--- source/day1.d
+++ source/day1.d
@@ -1,3 +1,4 @@
+import fluent.asserts;
 import std.algorithm;
 import std.range;
 
@@ -16,14 +17,14 @@ auto part1(T)(T lines) {
 }
 
 unittest {
-    assert(3 == part1(["1122"]),
-           "1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the second digit and the third digit (2) matches the fourth digit.");
-    assert(4 == part1(["1111"]),
-           "1111 produces 4 because each digit (all 1) matches the next.");
-    assert(0 == part1(["1234"]),
-           "1234 produces 0 because no digit matches the next.");
-    assert(9 == part1(["91212129"]),
-           "91212129 produces 9 because the only digit that matches the next one is the last digit, 9.");
+    ["1122"].part1.should.equal(3)
+        .because("1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the second digit and the third digit (2) matches the fourth digit.");
+    ["1111"].part1.should.equal(4)
+        .because("1111 produces 4 because each digit (all 1) matches the next.");
+    ["1234"].part1.should.equal(0)
+        .because("1234 produces 0 because no digit matches the next.");
+    ["91212129"].part1.should.equal(9)
+        .because("91212129 produces 9 because the only digit that matches the next one is the last digit, 9.");
 }
 
 auto part2(T)(T lines) {
@@ -34,14 +35,14 @@ auto part2(T)(T lines) {
 }
 
 unittest {
-    assert(6 == part2(["1212"]),
-           "1212 produces 6: the list contains 4 items, and all four digits match the digit 2 items ahead.");
-    assert(0 == part2(["1221"]),
-           "1221 produces 0, because every comparison is between a 1 and a 2.");
-    assert(4 == part2(["123425"]),
-           "123425 produces 4, because both 2s match each other, but no other digit has a match.");
-    assert(12 == part2(["123123"]),
-           "123123 produces 12.");
-    assert(4 == part2(["12131415"]),
-           "12131415 produces 4.");
+    ["1212"].part2.should.equal(6)
+        .because("1212 produces 6: the list contains 4 items, and all four digits match the digit 2 items ahead.");
+    ["1221"].part2.should.equal(0)
+        .because("1221 produces 0, because every comparison is between a 1 and a 2.");
+    ["123425"].part2.should.equal(4)
+        .because("123425 produces 4, because both 2s match each other, but no other digit has a match.");
+    ["123123"].part2.should.equal(12)
+        .because("123123 produces 12.");
+    ["12131415"].part2.should.equal(4)
+        .because("12131415 produces 4.");
 }
