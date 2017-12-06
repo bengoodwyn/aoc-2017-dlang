e04f23d8880fbda7d8b335e69f9aaa47c3a49576
Ben Goodwyn <ben@clouddirective.com>
2017-12-06 08:34:26 -0600

    :gear: Only run in CTFE in ctfe configuration

--- source/day1_ctfe.d
+++ source/day1_ctfe.d
@@ -1,12 +1,14 @@
-import std.string;
-import day1;
+version(ctfe) {
+    import std.string;
+    import day1;
 
-immutable input = [mixin(`"` ~ import("inputs/day1.txt") ~ `".strip`)];
+    immutable input = [mixin(`"` ~ import("inputs/day1.txt") ~ `".strip`)];
 
-immutable part1_result = part1(input);
-pragma(msg, "CTFE Day 1 Part 1: ", part1_result);
-static assert(1177 == part1_result);
+    immutable part1_result = part1(input);
+    pragma(msg, "CTFE Day 1 Part 1: ", part1_result);
+    static assert(1177 == part1_result);
 
-immutable part2_result = part2(input);
-pragma(msg, "CTFE Day 1 Part 2: ", part2_result);
-static assert(1060 == part2_result);
+    immutable part2_result = part2(input);
+    pragma(msg, "CTFE Day 1 Part 2: ", part2_result);
+    static assert(1060 == part2_result);
+}
--- source/day2_ctfe.d
+++ source/day2_ctfe.d
@@ -1,12 +1,14 @@
-import std.string;
-import day2;
+version(ctfe) {
+    import std.string;
+    import day2;
 
-immutable input = mixin(`"` ~ import("inputs/day2.txt") ~ `"`).splitLines;
+    immutable input = mixin(`"` ~ import("inputs/day2.txt") ~ `"`).splitLines;
 
-immutable part1_result = part1(input);
-pragma(msg, "CTFE Day 2 Part 1: ", part1_result);
-static assert(48357 == part1_result);
+    immutable part1_result = part1(input);
+    pragma(msg, "CTFE Day 2 Part 1: ", part1_result);
+    static assert(48357 == part1_result);
 
-immutable part2_result = part2(input);
-pragma(msg, "CTFE Day 2 Part 2: ", part2_result);
-static assert(351 == part2_result);
+    immutable part2_result = part2(input);
+    pragma(msg, "CTFE Day 2 Part 2: ", part2_result);
+    static assert(351 == part2_result);
+}
