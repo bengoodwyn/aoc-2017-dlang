5386580a600b7636a34d8242b65074dd7623d29a
Ben Goodwyn <ben@clouddirective.com>
2017-12-05 23:34:12 -0600

    :star: Day 6

---
+++ source/day6.d
@@ -0,0 +1,62 @@
+version(unittest) import fluent.asserts;
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.range;
+import std.stdio;
+import std.string;
+import std.typecons;
+
+char separator = '\t';
+
+void redistribute(ref int[] banks) {
+    int max_index = to!int(banks.maxIndex);
+    int length = to!int(banks.length);
+    int index = (max_index+1)%length;
+    int blocks_to_distribute = banks[max_index];
+    banks[max_index]=0;
+    while (blocks_to_distribute > 0) {
+        ++banks[index];
+        index = (index+1)%length;
+        --blocks_to_distribute;
+    }
+}
+
+auto to_banks(T)(T lines) {
+    return lines
+            .map!strip
+            .map!(to!int)
+            .array;
+}
+
+auto redistribute_until_repeat(int[] banks) {
+    int[string] known_banks;
+    int count = 0;
+    do {
+        known_banks[banks.to!string] = count;
+        redistribute(banks);
+        ++count;
+    } while (banks.to!string !in known_banks);
+
+    return tuple(count, count - known_banks[banks.to!string]);
+}
+
+auto part1(T)(T lines) {
+    return lines
+        .to_banks
+        .redistribute_until_repeat[0];
+}
+
+unittest {
+    ["0","2","7","0"].part1.should.equal(5);
+}
+
+auto part2(T)(T lines) {
+    return lines
+        .to_banks
+        .redistribute_until_repeat[1];
+}
+
+unittest {
+    ["0","2","7","0"].part2.should.equal(4);
+}
